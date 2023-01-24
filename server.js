d8736118b24c94f095601702590e7bbd852dd7f1
Kenneth Holt <kingholt@gmail.com>
2023-01-24 14:58:32 -0800

    updated files

--- server.js
+++ server.js
@@ -8,42 +8,137 @@ const app = express();
 const Book = require('./models/book.js');
 
 
-// const app = express();
+// middleware
+
 app.use(cors());
+app.use(express.json());
+
+
+// all that is needed to connect to library
 mongoose.connect(process.env.DB_URL);
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  console.log('Mongoose is live!');
 
-});
 
+// confirm connection
+
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function () {
+// console.log('Mongoose is live!');
+// 
+// });
+
+
+//  define port
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (request, response) => {
+
+//  route
+app.get('/', (request, response, next) => {
 
   response.send('test request received');
 
 });
 
+// call routes
 app.get('/book', getBooks);
+app.get('')
 
+// code that runs out of order, waiting on model
+async function getBooks(request, response, next) {
 
-async function getBooks(request, response) {
   try {
     let dbBooks = await Book.find();
     response.status(200).send(dbBooks);
 
   } catch (error) {
-    console.log(error.message);
-    response.status(400).send('Bad Request');
+    next(error);
+
+  }
+}
+
+
+async function postBook(request, response, next) {
+  console.log(request.body);
+
+  try {
+    let createBook = await Book.create(request.body);
+    response.status(200).send(createBook);
+
+  } catch (error) {
+    next(error);
+  }
+}
+
+
+async function deleteBook(request, response, next) {
+  let id = request.params.id;
+
+  try {
+    await Book.findByIdAndDelete(id);
+    response.status(200).send('The Book has been removed.');
+
+  } catch (error) {
+    next(error);
+  }
+}
+
+
+async function putBook(request, response, next) {
+  let id = request.params.id;
+
+  try {
+    let data = request.body;
+    let updateBook = await Book.findByIdAndUpdate(id, data, { new: true, overwrite: true});
+    response.status(200).send(updateBook);
+
+  } catch (error) {
+    next(error);
 
   }
 }
 
 app.get('*', (request, response) => {
-  response.status(404).send('Sorry, this page does not exist.');
+  response.status(404).send('This one is not available.');
 
 });
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
+
+});
+
+
+
+app.listen(PORT, () => console.log(`Loud and clear on:  ${PORT}`));
+
+
+
+  // endpoint to delete
+  // must have path parameter
+  // use a variable to capture id
+  // create variable with colon
+  // app.delete('/modules/:bookID', deleteBook);
+
+  // async function deleteBook(request, response, next) {
+
+    // try {
+      // let
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
